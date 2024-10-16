05dd32781549c1d7c471ab41ad0dda0bfd272a47
aluminumlizardman <aluminumlizardman@gmail.com>
2024-10-17 01:01:36 +1100

    new push

---
+++ src/LizardFreeCamera.h
@@ -0,0 +1,151 @@
+/**********************************************************************************************
+*
+*   raylibExtras * Utilities and Shared Components for Raylib
+*
+*   RLAssets * Simple Asset Managment System for Raylib
+*
+*   LICENSE: MIT
+*
+*   Copyright (c) 2020 Jeffery Myers
+*
+*   Permission is hereby granted, free of charge, to any person obtaining a copy
+*   of this software and associated documentation files (the "Software"), to deal
+*   in the Software without restriction, including without limitation the rights
+*   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+*   copies of the Software, and to permit persons to whom the Software is
+*   furnished to do so, subject to the following conditions:
+*
+*   The above copyright notice and this permission notice shall be included in all
+*   copies or substantial portions of the Software.
+*
+*   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+*   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+*   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+*   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+*   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+*   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+*   SOFTWARE.
+*
+**********************************************************************************************/
+
+#pragma once
+
+#include "raylib.h"
+#include "raymath.h"
+
+
+
+Camera ViewCam = { 0 };
+Vector2 LastMousePos = { 0 };
+
+Vector3 previousCameraPosition = { 0.0f, 0.0f, 0.0f };
+
+float MoveSpeed = 10;
+
+int ForwardKey = KEY_W;
+int BackwardKey = KEY_S;
+int LeftKey = KEY_A;
+int RightKey = KEY_D;
+
+enum CameraMode
+{
+    EditMode,
+    FPSMode,
+    LockedMode,
+};
+
+inline void InitLizardFreeCam(float fovy)
+{
+    ViewCam.fovy = fovy;
+    ViewCam.projection = CAMERA_PERSPECTIVE;
+    ViewCam.up.y = 1;
+    ViewCam.target.z = 1;
+    
+    LastMousePos = GetMousePosition(); 
+}
+
+inline void UpdateLizardFreeCam(int MODE, Vector3 POS)
+{
+    previousCameraPosition = ViewCam.position;
+    static float pitch = 0.0f;
+    static float yaw = 0.0f;
+
+    if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT) || MODE == FPSMode)
+    {
+        if (IsKeyDown(KEY_LEFT_SHIFT))
+        {
+            MoveSpeed = 33;
+            if (IsKeyDown(KEY_X))
+            {
+                MoveSpeed = 9999;
+            }
+        }
+        else
+        {
+            MoveSpeed = 10;
+        }
+
+        float degPerPixel = ViewCam.fovy / GetScreenHeight();
+        Vector2 delta = Vector2Scale(Vector2Subtract(LastMousePos, GetMousePosition()), degPerPixel);
+
+        // Update pitch and yaw
+        pitch += delta.y;  // Change the sign to correct the inversion
+        yaw += delta.x;
+
+        // Clamp the pitch to prevent looking all the way up or down
+        float pitchLimit = 89.0f;
+        if (pitch > pitchLimit) pitch = pitchLimit;
+        if (pitch < -pitchLimit) pitch = -pitchLimit;
+
+        // Calculate forward direction based on pitch and yaw
+        Vector3 forward;
+        forward.x = cosf(DEG2RAD * pitch) * sinf(DEG2RAD * yaw);
+        forward.y = sinf(DEG2RAD * pitch);
+        forward.z = cosf(DEG2RAD * pitch) * cosf(DEG2RAD * yaw);
+        forward = Vector3Normalize(forward);
+
+        Vector3 right = Vector3CrossProduct(forward, (Vector3) { 0, 1, 0 });
+        right = Vector3Normalize(right);
+        Vector3 up = Vector3CrossProduct(right, forward);
+        up = Vector3Normalize(up);
+
+        float forwardSpeed = 0;
+        if (IsKeyDown(ForwardKey))
+            forwardSpeed = 1;
+        else if (IsKeyDown(BackwardKey))
+            forwardSpeed = -1;
+
+        float sideSpeed = 0;
+        if (IsKeyDown(RightKey))
+            sideSpeed = 1;
+        else if (IsKeyDown(LeftKey))
+            sideSpeed = -1;
+
+        float upSpeed = 0;
+        if (IsKeyDown(KEY_SPACE))
+            upSpeed = 1;
+        else if (IsKeyDown(KEY_LEFT_CONTROL))
+            upSpeed = -1;
+
+        forwardSpeed *= GetFrameTime() * MoveSpeed;
+        sideSpeed *= GetFrameTime() * MoveSpeed;
+        upSpeed *= GetFrameTime() * MoveSpeed;
+
+        if (MODE == EditMode)
+        {
+            ViewCam.position = Vector3Add(ViewCam.position, Vector3Scale(forward, forwardSpeed));
+            ViewCam.position = Vector3Add(ViewCam.position, Vector3Scale(right, sideSpeed));
+            ViewCam.position.y += upSpeed; //Vector3Add(ViewCam.position, Vector3Scale(up, upSpeed));
+        }
+        else if (MODE == FPSMode)
+        {
+            ViewCam.position = POS;
+            DisableCursor();
+        }
+
+        ViewCam.target = Vector3Add(ViewCam.position, forward);
+    }
+    LastMousePos = GetMousePosition();
+    
+}
+
--- src/raylib_game.c
+++ src/raylib_game.c
@@ -12,7 +12,7 @@
 ********************************************************************************************/
 
 #include "raylib.h"
-
+#include "LizardFreeCamera.h"
 #if defined(PLATFORM_WEB)
     #define CUSTOM_MODAL_DIALOGS            // Force custom modal dialogs usage
     #include <emscripten/emscripten.h>      // Emscripten library - LLVM to JavaScript compiler
@@ -22,11 +22,7 @@
 #include <stdlib.h>                         // Required for: 
 #include <string.h>                         // Required for: 
 
-//----------------------------------------------------------------------------------
-// Defines and Macros
-//----------------------------------------------------------------------------------
-// Simple log system to avoid printf() calls if required
-// NOTE: Avoiding those calls, also avoids const strings memory usage
+
 #define SUPPORT_LOG_INFO
 #if defined(SUPPORT_LOG_INFO)
     #define LOG(...) printf(__VA_ARGS__)
@@ -34,120 +30,112 @@
     #define LOG(...)
 #endif
 
-//----------------------------------------------------------------------------------
-// Types and Structures Definition
-//----------------------------------------------------------------------------------
-typedef enum { 
-    SCREEN_LOGO = 0, 
-    SCREEN_TITLE, 
-    SCREEN_GAMEPLAY, 
-    SCREEN_ENDING
-} GameScreen;
-
-// TODO: Define your custom data types here
-
-//----------------------------------------------------------------------------------
-// Global Variables Definition
-//----------------------------------------------------------------------------------
 static const int screenWidth = 1280;
 static const int screenHeight = 720;
 
 static RenderTexture2D target = { 0 };  // Render texture to render our game
 
-// TODO: Define global variables here, recommended to make them static
+void UpdateDrawFrame(void)
+{
 
-//----------------------------------------------------------------------------------
-// Module Functions Declaration
-//----------------------------------------------------------------------------------
-static void UpdateDrawFrame(void);      // Update and Draw one frame
+    UpdateLizardFreeCam(EditMode, Vector3Zero());
 
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
+    if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT))
+    {
+        LastMousePos = (Vector2){ GetScreenWidth() / 2, GetScreenHeight() / 2 };
+        SetMousePosition(GetScreenWidth() / 2, GetScreenHeight() / 2);
+        DisableCursor();
+    }
+    if (IsMouseButtonReleased(MOUSE_BUTTON_RIGHT))
+    {
+        SetMousePosition(GetScreenWidth() / 2, GetScreenHeight() / 2);
+        EnableCursor();
+    }
+
+
+
+    BeginTextureMode(target);
+
+        ClearBackground(RAYWHITE);
+        
+
+
+    EndTextureMode();
+    
+
+   /* BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawTexturePro(target.texture, (Rectangle){ 0, 0, (float)target.texture.width, -(float)target.texture.height }, (Rectangle){ 0, 0, (float)target.texture.width, (float)target.texture.height }, (Vector2){ 0, 0 }, 0.0f, WHITE);
+    EndDrawing();*/
+
+    //----------------------------------------------------------------------------------
+    BeginDrawing();
+
+    ClearBackground(RAYWHITE);
+
+    BeginMode3D(ViewCam);
+
+    DrawCube(Vector3Zero(), 2.0f, 2.0f, 2.0f, RED);
+    DrawCubeWires(Vector3Zero(), 2.0f, 2.0f, 2.0f, MAROON);
+
+    DrawGrid(10, 1.0f);
+
+    EndMode3D();
+
+    DrawRectangle(10, 10, 320, 93, Fade(SKYBLUE, 0.5f));
+    DrawRectangleLines(10, 10, 320, 93, BLUE);
+
+    DrawText("Free camera default controls:", 20, 20, 10, BLACK);
+    DrawText("- Mouse Wheel to Zoom in-out", 40, 40, 10, DARKGRAY);
+    DrawText("- Mouse Wheel Pressed to Pan", 40, 60, 10, DARKGRAY);
+    DrawText("- Z to zoom to (0, 0, 0)", 40, 80, 10, DARKGRAY);
+
+
+    if (IsKeyDown(KEY_F))
+    {
+        DrawText("Hopefully it works - Lizard, 2024", 20, GetScreenHeight() -20, 20, RED);
+    }
+    else
+    {
+        DrawText("Hopefully it works - Lizard, 2024", 20, GetScreenHeight() - 30, 20, BLUE);
+    }
+
+    EndDrawing();
+
+
+
+    //----------------------------------------------------------------------------------  
+}
+
+
+
 int main(void)
 {
 #if !defined(_DEBUG)
     SetTraceLogLevel(LOG_NONE);         // Disable raylib trace log messages
 #endif
 
-    // Initialization
-    //--------------------------------------------------------------------------------------
     InitWindow(screenWidth, screenHeight, "raylib gamejam template");
-    
-    // TODO: Load resources / Initialize variables at this point
-    
-    // Render texture to draw full screen, enables screen scaling
-    // NOTE: If screen is scaled, mouse input should be scaled proportionally
+
     target = LoadRenderTexture(screenWidth, screenHeight);
     SetTextureFilter(target.texture, TEXTURE_FILTER_BILINEAR);
+    InitLizardFreeCam(70.0f);
 
 #if defined(PLATFORM_WEB)
     emscripten_set_main_loop(UpdateDrawFrame, 60, 1);
 #else
     SetTargetFPS(60);     // Set our game frames-per-second
-    //--------------------------------------------------------------------------------------
+    
+    
 
-    // Main game loop
     while (!WindowShouldClose())    // Detect window close button
     {
         UpdateDrawFrame();
     }
 #endif
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
     UnloadRenderTexture(target);
-    
-    // TODO: Unload all loaded resources at this point
 
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
-
+    CloseWindow();
     return 0;
-}
-
-//--------------------------------------------------------------------------------------------
-// Module functions definition
-//--------------------------------------------------------------------------------------------
-// Update and draw frame
-void UpdateDrawFrame(void)
-{
-    // Update
-    //----------------------------------------------------------------------------------
-    // TODO: Update variables / Implement example logic at this point
-    //----------------------------------------------------------------------------------
-
-    // Draw
-    //----------------------------------------------------------------------------------
-    // Render game screen to a texture, 
-    // it could be useful for scaling or further shader postprocessing
-    BeginTextureMode(target);
-        ClearBackground(RAYWHITE);
-        
-        DrawRectangle(10, 10, screenWidth - 20, screenHeight - 20, SKYBLUE);
-        if (IsKeyDown(KEY_F))
-        {
-            DrawText("Hopefully it works - Lizard, 2024", GetScreenWidth() / 2, GetScreenHeight() / 2, 20, RED);
-        }
-        else
-        {
-            DrawText("Hopefully it works - Lizard, 2024", GetScreenWidth() / 2, GetScreenHeight() / 2, 20, BLUE);
-        }
-        // TODO: Draw your game screen here
-        
-        
-        
-    EndTextureMode();
-    
-    // Render to screen (main framebuffer)
-    BeginDrawing();
-        ClearBackground(RAYWHITE);
-        
-        // Draw render texture to screen, scaled if required
-        DrawTexturePro(target.texture, (Rectangle){ 0, 0, (float)target.texture.width, -(float)target.texture.height }, (Rectangle){ 0, 0, (float)target.texture.width, (float)target.texture.height }, (Vector2){ 0, 0 }, 0.0f, WHITE);
-
-        // TODO: Draw everything that requires to be drawn at this point, maybe UI?
-
-    EndDrawing();
-    //----------------------------------------------------------------------------------  
 }
